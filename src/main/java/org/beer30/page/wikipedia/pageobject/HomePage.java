604576ac7d46224b21614df5a26b1e207e43f820
Tony Sweets <tsweets@readyfunds.net>
2013-10-12 21:29:46 -0600

    added more comments

--- src/main/java/org/beer30/page/wikipedia/pageobject/HomePage.java
+++ src/main/java/org/beer30/page/wikipedia/pageobject/HomePage.java
@@ -13,19 +13,32 @@ import org.slf4j.LoggerFactory;
  *         Date: 9/30/13
  *         Time: 8:24 PM
  */
+// This is the Home Page Class which when created (When the test is ran) becomes a Page Object
 public class HomePage {
+    // This is a logging utility. Used to log to a file and/or the screen
     private static final Logger log = LoggerFactory.getLogger(HomePage.class);
+
+    // This is the Object that acts as a web browser when using Html Unit
+    // or the thing that "drives" a real web browser like Firefox
     private WebDriver webDriver;
+
+    // These are elements (Links, Input Boxes, etc) on this page that I need to use
     private WebElement privacyPolicyLink;
     private WebElement searchBox;
     private WebElement submitButton;
 
+    // Constructor of this Page Object
     public HomePage(WebDriver webDriver) {
         log.info("Page  = {}", webDriver.getTitle());
         log.info("URL   = {}", webDriver.getCurrentUrl());
         this.webDriver = webDriver;
+
+        // Test that the page that the page we are on is actually the Wikipedia Home Page.
+        // We know that we are on the home page if the title matches the single word
+        // "Wikipedia" and nothing else
         if (!webDriver.getTitle().equals("Wikipedia")) {
-            throw new IllegalStateException("Requested HomePage: current page - " + webDriver.getTitle());
+            throw new IllegalStateException
+                    ("Requested HomePage: current page - " + webDriver.getTitle());
         }
 
         //Initialize WebElements
@@ -33,7 +46,6 @@ public class HomePage {
         searchBox = webDriver.findElement(By.id("searchInput"));
         submitButton = webDriver.findElement(By.name("go"));
     }
-
 
     public PrivacyPolicyPage clickPrivacyPolicyLink() {
         privacyPolicyLink.click();
